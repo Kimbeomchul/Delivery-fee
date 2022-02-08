31d9453f4ee565e8a6a00abd6546f116536ca5a2
JunKi Yoon <na66421@gmail.com>
2022-02-08 21:54:25 +0900

    Postgresql 추가

--- delivery-backend/delivery/delivery/settings.py
+++ delivery-backend/delivery/delivery/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "delivery",
+        'USER': "delivery_user",
+        'PASSWORD': "delivery_password",
+        'HOST': "127.0.0.1",
+        'PORT': '5432',
     }
 }
 
