c2e84d5c20d7b945d1d1dbd1075a0bcefb2dafed
JunKi Yoon <na66421@gmail.com>
2022-02-08 22:02:59 +0900

    postgres 유저정보 변경

--- delivery-backend/delivery/delivery/settings.py
+++ delivery-backend/delivery/delivery/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "delivery",
-        'USER': "delivery_user",
-        'PASSWORD': "delivery_password",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
         'HOST': "127.0.0.1",
         'PORT': '5432',
     }
