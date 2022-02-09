1db281fd101c0d824fc2a47545ae5172d8623665
JunKi Yoon <na66421@gmail.com>
2022-02-09 20:32:47 +0900

    댓글창 hide-details 수정

--- delivery-front/src/components/Comment.vue
+++ delivery-front/src/components/Comment.vue
@@ -1,26 +1,26 @@
 <template>
-        <v-flex xs12 sm6 md3>
-          <v-text-field label="댓글을 입력해주세요." type="text" outlined dense v-model="message">
-          <template v-slot:append>
-            <v-fade-transition leave-absolute>
-                <v-progress-circular
-                    v-if="loading"
-                    size="24"
-                    color="info"
-                    indeterminate
-                ></v-progress-circular>
-                <img
-                    v-else
-                    width="24"
-                    height="24"
-                    src="../assets/btn.png"
-                    alt="btn"
-                />
-            </v-fade-transition>
-        </template>
-          </v-text-field>
-        </v-flex>
-
+    <v-flex xs12 sm6 md3>
+        <v-text-field
+            label="댓글을 입력해주세요."
+            type="text"
+            hide-details
+            outlined
+            dense
+            v-model="message"
+        >
+            <template v-slot:append>
+                <v-fade-transition leave-absolute>
+                    <v-progress-circular
+                        v-if="loading"
+                        size="24"
+                        color="info"
+                        indeterminate
+                    ></v-progress-circular>
+                    <img v-else width="24" height="24" src="../assets/btn.png" alt="btn" />
+                </v-fade-transition>
+            </template>
+        </v-text-field>
+    </v-flex>
 </template>
 
 <script>
@@ -30,15 +30,15 @@ export default {
 </script>
 <style scoped>
 .v-text-field--outlined >>> fieldset {
-  position: absolute;
-  border-color: white;
-  background-color: #E5E5E5;
-  z-index: 0;
+    position: absolute;
+    border-color: white;
+    background-color: #e5e5e5;
+    z-index: 0;
 }
-.v-text-field{
+.v-text-field {
     border-radius: 12px;
 }
-.v-text-field >>> img{
+.v-text-field >>> img {
     z-index: 1;
 }
 </style>
