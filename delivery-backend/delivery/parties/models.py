b6e7b008225c476f5775ca568a2eeb6af4b82e34
JunKi Yoon <na66421@gmail.com>
2022-02-24 22:30:56 +0900

    유저 모델에 내위치, 위도경도 저장

--- delivery-backend/delivery/parties/models.py
+++ delivery-backend/delivery/parties/models.py
@@ -1,6 +1,5 @@
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import Point
 
 from taggit.managers import TaggableManager
 
@@ -13,6 +12,5 @@ class Party(Timestampable):
     tags = TaggableManager()
     order_time = models.DateTimeField()
     content = models.TextField(help_text='파티 내용')
-    location = models.PointField(default=Point(0.0, 0.0))
     user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='파티 주최자')
     participants = ArrayField(models.IntegerField(), default=list, help_text='파티 참여자 명단')
--- delivery-backend/delivery/users/models.py
+++ delivery-backend/delivery/users/models.py
@@ -1,5 +1,6 @@
-from django.contrib.gis.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 
 from common.models import Timestampable
 
@@ -9,6 +10,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 class User(AbstractUser, Timestampable):
     name = models.CharField(max_length=200, help_text='사용자 이름')
     phone_number = PhoneNumberField()
+    address = models.CharField(max_length=200, blank=True, help_text='사용자 내위치')
+    location = models.PointField(default=Point(0.0, 0.0), help_text='위도 경도')
 
     class Meta:
         ordering = ['-id']
