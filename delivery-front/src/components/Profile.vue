370a32a4c74a88551b83a2636d7d760836386493
JunKi Yoon <na66421@gmail.com>
2022-03-10 22:26:44 +0900

    프로필 페이지 수정

--- delivery-front/src/components/Profile.vue
+++ delivery-front/src/components/Profile.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
-        <v-divider class="pb-5"></v-divider>
-        <v-divider class="pb-5"></v-divider>
+        <v-row justify="center">
+            <v-col cols="2">
+                <v-avatar color="grey lighten-2" size="45"></v-avatar>
+            </v-col>
+            <v-col cols="10">
+                <p class="text-h5 text-right">거친 빽뺵도요님</p>
+            </v-col>
+        </v-row>
+        <v-divider class="mt-3 mb-3"></v-divider>
+        <div class="text-body-1 text--primary">약관 및 정책</div>
+        <v-divider class="mt-3 mb-3"></v-divider>
+        <div class="text-body-1 text--primary">약관 및 정책</div>
+        <v-divider class="mt-3 mb-3"></v-divider>
+        <div class="text-body-1 text--primary">약관 및 정책</div>
+        <v-divider class="mt-3 mb-3"></v-divider>
+        <div class="text-body-1 text--primary">약관 및 정책</div>
+        <v-divider class="mt-3 mb-3"></v-divider>
     </div>
 </template>
 
