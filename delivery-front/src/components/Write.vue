ae1cfcbdfb80dabf32683bd370ae0cb0c6a8975e
JunKi Yoon <na66421@gmail.com>
2022-03-16 21:32:25 +0900

    Add from validation

--- delivery-front/src/components/Write.vue
+++ delivery-front/src/components/Write.vue
@@ -1,78 +1,81 @@
 <template>
     <div>
-        <div class="font-weight-black" style="color: #52d4dc">제목</div>
-        <v-text-field v-model="title" placeholder="제목을 입력해주세요."></v-text-field>
-        <div class="font-weight-black" style="color: #52d4dc">종류</div>
-        <v-item-group multiple max="3" v-model="selectedTags">
-            <v-item v-for="(item, index) in tags" :key="index" :value="item" v-slot="{ active, toggle }">
-                <v-chip small class="ma-1" active-class="blue--text" :input-value="active" @click="toggle">
-                    # {{ item }}
-                </v-chip>
-            </v-item>
-        </v-item-group>
-        <br />
-        <div class="font-weight-black" style="color: #52d4dc">주문하고 싶은 시간대를 선택해주세요</div>
-        <div class="text-right text-caption" v-text="timerSlider.val + '분후'">분후</div>
-        <v-slider
-            v-model="timerSlider.val"
-            :thumb-color="timerSlider.color"
-            :thumb-label="true"
-            step="10"
-            max="120"
-            ticks
-            dense
-            hide-details
-        ></v-slider>
-        <div class="font-weight-black text-right text-body2">
-            {{ orderTime.format("A HH시 mm분") }}
-        </div>
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <div class="font-weight-black" style="color: #52d4dc">제목</div>
+            <v-text-field v-model="title" :rules="titleRules" placeholder="제목을 입력해주세요."></v-text-field>
+            <div class="font-weight-black" style="color: #52d4dc">종류</div>
+            <v-item-group multiple max="3" v-model="selectedTags">
+                <v-item v-for="(item, index) in tags" :key="index" :value="item" v-slot="{ active, toggle }">
+                    <v-chip small class="ma-1" active-class="blue--text" :input-value="active" @click="toggle">
+                        # {{ item }}
+                    </v-chip>
+                </v-item>
+            </v-item-group>
+            <br />
+            <div class="font-weight-black" style="color: #52d4dc">주문하고 싶은 시간대를 선택해주세요</div>
+            <div class="text-right text-caption" v-text="timerSlider.val + '분후'">분후</div>
+            <v-slider
+                v-model="timerSlider.val"
+                :thumb-color="timerSlider.color"
+                :thumb-label="true"
+                step="10"
+                max="120"
+                ticks
+                dense
+                hide-details
+            ></v-slider>
+            <div class="font-weight-black text-right text-body2">
+                {{ orderTime.format("A HH시 mm분") }}
+            </div>
 
-        <div class="font-weight-black" style="color: #52d4dc">내용</div>
-        <v-textarea
-            name="input-7-1"
-            rows="6"
-            filled
-            no-resize
-            placeholder="내용을 입력해주세요."
-            v-model="content"
-        ></v-textarea>
-        <v-row>
-            <v-col cols="3">
-                <div class="font-weight-black" style="color: #52d4dc">내위치</div>
-            </v-col>
-            <v-col cols="9">
-                <div class="text-caption font-weight-black text-right">경기도 과천시 관문로 106 104동 1010호</div>
-            </v-col>
-        </v-row>
-        <br />
-        <v-btn
-            v-if="!$route.query.edit"
-            bottom
-            block
-            x-large
-            rounded
-            color="#52D4DC"
-            dark
-            class="font-weight-bold"
-            style="font-size: 1.02em"
-            @click="createParty()"
-        >
-            작성하기
-        </v-btn>
-        <v-btn
-            v-else
-            bottom
-            block
-            x-large
-            rounded
-            color="#52D4DC"
-            dark
-            class="font-weight-bold"
-            style="font-size: 1.02em"
-            @click="editParty()"
-        >
-            수정하기
-        </v-btn>
+            <div class="font-weight-black" style="color: #52d4dc">내용</div>
+            <v-textarea
+                name="input-7-1"
+                rows="7"
+                filled
+                no-resize
+                :rules="contentRules"
+                v-model="content"
+                placeholder="내용을 입력해주세요."
+            ></v-textarea>
+            <v-row>
+                <v-col cols="3">
+                    <div class="font-weight-black" style="color: #52d4dc">내위치</div>
+                </v-col>
+                <v-col cols="9">
+                    <div class="text-caption font-weight-black text-right">경기도 과천시 관문로 106 104동 1010호</div>
+                </v-col>
+            </v-row>
+            <br />
+            <v-btn
+                v-if="!$route.query.edit"
+                bottom
+                block
+                x-large
+                rounded
+                color="#52D4DC"
+                dark
+                class="font-weight-bold"
+                style="font-size: 1.02em"
+                @click="createParty()"
+            >
+                작성하기
+            </v-btn>
+            <v-btn
+                v-else
+                bottom
+                block
+                x-large
+                rounded
+                color="#52D4DC"
+                dark
+                class="font-weight-bold"
+                style="font-size: 1.02em"
+                @click="editParty()"
+            >
+                수정하기
+            </v-btn>
+        </v-form>
     </div>
 </template>
 
@@ -90,6 +93,9 @@ export default {
         selectedTags: "",
         timerSlider: { val: 0, color: "#52D4DC" },
         editTimerSlider: { val: 0, color: "#52D4DC" },
+        valid: true,
+        titleRules: [(value) => !!value || "제목은 필수 입력 항목입니다."],
+        contentRules: [(value) => !!value || "내용은 필수 입력 항목입니다."],
     }),
     created: function () {
         this.allowEditParty();
@@ -121,6 +127,9 @@ export default {
     },
     methods: {
         createParty: async function () {
+            const validate = this.$refs.form.validate();
+            if (!validate) return;
+
             const data = {
                 title: this.title,
                 tags: this.selectedTags,
@@ -142,6 +151,9 @@ export default {
             }
         },
         editParty: async function () {
+            const validate = this.$refs.form.validate();
+            if (!validate) return;
+
             const data = {
                 title: this.title,
                 tags: this.selectedTags,
